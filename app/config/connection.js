6433b13cc90e5fcc5881acead4ec122fd570f932
QuantumT95 <tran.charlie@ymail.com>
2017-11-11 13:20:03 -0600

    took out korwynns password and left it blank in config.json and connection.js

--- app/config/connection.js
+++ app/config/connection.js
@@ -6,7 +6,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var connection = new Sequelize("tennis_db", "root", "Wagstaff2017", {
+var connection = new Sequelize("tennis_db", "root", "", {
   host: "localhost",
   dialect: "mysql",
   pool: {
