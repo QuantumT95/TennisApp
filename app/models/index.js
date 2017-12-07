0cc1389e03b4a1145b0a3aea3dffad1c281a4174
soniab <sonia.bradley524@gmail.com>
2017-12-07 17:35:43 -0600

    try again

--- app/models/index.js
+++ app/models/index.js
@@ -11,15 +11,15 @@ var db = {};
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-var sequelize = new Sequelize('tennis_db', 'root', 'MyNewPass', {
-  host: "127.0.0.1",
-  dialect: 'mysql',
-  define: {
-      timestamp: false
-  }
-});
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+// var sequelize = new Sequelize('tennis_db', 'root', 'MyNewPass', {
+//   host: "127.0.0.1",
+//   dialect: 'mysql',
+//   define: {
+//       timestamp: false
+//   }
+// });
 
 fs
   .readdirSync(__dirname)
@@ -40,4 +40,4 @@ Object.keys(db).forEach(function(modelName) {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-// module.exports = db;
+module.exports = db;
