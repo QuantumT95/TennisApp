579051c9619a68b5e82467dfd448a18ecdd8cd54
soniab <sonia.bradley524@gmail.com>
2017-12-07 12:13:31 -0600

    deploy with sequelize

--- app/config/connection.js
+++ app/config/connection.js
@@ -6,27 +6,15 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var connection;
-
-if (process.env.JAWSDB_URL) {
-  connection =mysql.createConnection(process.env.JAWSDB_URL);
-  } else {
-    connection = mysql.createConnection({
-      host: 'localhost',
-      username: 'root',
-      password: 'MyNewPass',
-      database: 'tennis_db'
-    });
+var connection = new Sequelize("tennis_db", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
-// = new Sequelize("tennis_db", "root", "", {
-//   host: "localhost",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   }
-// });
-connection.connect();
+});
+
 // Exports the connection for other files to use
 module.exports = connection;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var App = Express();
 App.use(bodyParser.urlencoded({ extended: false }));
 App.use(Express.static(__dirname + '/app/public'));
-var PORT = process.env.PORT || 3306;
+var PORT = process.env.PORT || 8082;
 var DB = require("./app/models");
 
 require("./app/routes/sql-routes.js")(App);
@@ -13,4 +13,4 @@ DB.sequelize.sync({ force: true }).then(function() {
   App.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-});
+}); 
